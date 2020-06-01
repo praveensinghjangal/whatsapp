7a97fd1a316125260ac8227103f7157cb759fd9e
Danish Galiyara <danish.galiyara@vivainfomedia.com>
2020-06-01 20:46:36 +0530

    added .env

--- server.js
+++ server.js
@@ -4,7 +4,11 @@
  */
 
 const _ = require('lodash')
-//process.env.BLUEBIRD_W_FORGOTTEN_RETURN = 0;
+// process.env.BLUEBIRD_W_FORGOTTEN_RETURN = 0;
+
+// LOAD ENV FILE START ==================================================
+if (process.env.NODE_ENV === 'development') require('dotenv').config({ path: process.env.PWD + '/config/.env' })
+// LOAD ENV FILE END ====================================================
 
 const SERVER_SCRIPTS = {
   http_api: './workers/http_api.js'
@@ -13,7 +17,7 @@ const SERVER_SCRIPTS = {
 const WORKER_TYPE = process.env.WORKER_TYPE || null
 if (_.isEmpty(WORKER_TYPE) && _.isEmpty(SERVER_SCRIPTS[WORKER_TYPE])) {
   console.log('no such WORKER_TYPE, possible values - ' + JSON.stringify(Object.keys(SERVER_SCRIPTS)))
-    process.exit(1)
+  process.exit(1)
 }
 console.log('Loaded config environment : ' + process.env.NODE_ENV)
 console.log('Loaded worker type : ' + WORKER_TYPE)
