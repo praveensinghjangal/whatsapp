effed3be84fdcc11e424adb3d8dab3a9cff55314
Danish Galiyara <danish.galiyara@vivainfomedia.com>
2020-07-22 19:54:34 +0530

    handling of handling of optin message and audience

--- app_modules/integration/service/audienceAndOptin.js
+++ app_modules/integration/service/audienceAndOptin.js
@@ -7,7 +7,7 @@ const isTyntecOptinMessage = (content, optinText) => {
   const isOptin = q.defer()
   if (content && content.contentType === 'text' && content.text && optinText) {
     content.text = content.text.trim()
-    if (content.text.length === optinText.length && content.text === optinText) {
+    if (content.text.length === optinText.length && content.text.toLowerCase() === optinText) {
       isOptin.resolve(true)
     } else {
       isOptin.resolve(false)
@@ -27,26 +27,28 @@ let isOptinMessage = () => {
 function addAudienceAndOptin (inputPayload, redisData) {
   const audienceData = q.defer()
   const url = __config.base_url + __constants.INTERNAL_END_POINTS.addupdateAudience
-  const audienceDataToBePosted = {
+  const audienceDataToBePosted = [{
     phoneNumber: inputPayload.from,
     channel: __constants.DELIVERY_CHANNEL.whatsapp,
     optinSourceId: __config.optinMessageSource,
     name: inputPayload.whatsapp && inputPayload.whatsapp.senderName ? inputPayload.whatsapp.senderName : ''
-  }
+  }]
   if (!audienceDataToBePosted.name) delete audienceDataToBePosted.name
   if (__config.provider_config[redisData.serviceProviderId].name === 'tyntec') {
     isOptinMessage = isTyntecOptinMessage
   }
   isOptinMessage(inputPayload.content, redisData.optinText)
     .then(isoptin => {
+      audienceDataToBePosted[0].optin = isoptin
       const options = {
         url,
         body: audienceDataToBePosted,
         headers: { Authorization: __config.internalApiCallToken },
         json: true
       }
-      audienceDataToBePosted.optin = isoptin
+      console.log('all options', options)
       request.post(options, (err, httpResponse, body) => {
+        console.log('hereeeeeeeeeeeeeeeeeeeeee', body)
         if (err) {
           audienceData.reject(err)
         } else {
