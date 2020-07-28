70bcfc0384cd1c4e51abe69cc8ae715ab2bf11b8
Danish Galiyara <danish.galiyara@vivainfomedia.com>
2020-07-28 10:52:56 +0530

    added check in mock api to send only icoming message

--- app_modules/web_hooks/controllers/mockCallBack.js
+++ app_modules/web_hooks/controllers/mockCallBack.js
@@ -4,37 +4,41 @@ const request = require('request')
 
 const controller = (req, res) => {
   // __logger.info('Input', req.body)
-  const inputData = []
-  __config.mockWebHook.receiverNumber.forEach(number => {
-    inputData.push({
-      to: number,
-      channels: [
-        'whatsapp'
-      ],
-      whatsapp: {
-        from: __config.mockWebHook.senderNumber,
-        contentType: 'text',
-        text: JSON.stringify(req.body)
-      }
+  if (req.body && req.body.event && (req.body.event.toLowerCase() === 'momessage' || req.body.event.toLowerCase() === 'momessage::postback')) {
+    const inputData = []
+    __config.mockWebHook.receiverNumber.forEach(number => {
+      inputData.push({
+        to: number,
+        channels: [
+          'whatsapp'
+        ],
+        whatsapp: {
+          from: __config.mockWebHook.senderNumber,
+          contentType: 'text',
+          text: JSON.stringify(req.body)
+        }
+      })
     })
-  })
 
-  const url = __config.base_url + __constants.INTERNAL_END_POINTS.sendMessageToQueue
-  // __logger.info('Url>>>>>>>>>>>>>>>>>>>>>>>>', typeof url)
-  console.log('..........................', inputData)
-  const options = {
-    url,
-    body: inputData,
-    headers: { Authorization: __config.mockWebHook.authorization },
-    json: true
-  }
-  // Calling another api for sending messages
-  request.post(options, (err, httpResponse, body) => {
-    if (err) {
-      return res.send(err)
+    const url = __config.base_url + __constants.INTERNAL_END_POINTS.sendMessageToQueue
+    // __logger.info('Url>>>>>>>>>>>>>>>>>>>>>>>>', typeof url)
+    console.log('..........................', inputData)
+    const options = {
+      url,
+      body: inputData,
+      headers: { Authorization: __config.mockWebHook.authorization },
+      json: true
     }
-    return res.send(body)
-  })
+    // Calling another api for sending messages
+    request.post(options, (err, httpResponse, body) => {
+      if (err) {
+        return res.send(err)
+      }
+      return res.send(body)
+    })
+  } else {
+    res.status(202).send()
+  }
 }
 
 module.exports = controller
