0faa7eb264935cc93657ba13cc7b67ad287bc8cd
Arjun Bhole <arjun.bhole@vivainfomedia.com>
2020-08-07 13:34:28 +0530

    did tps and template redis servie

--- app_modules/templates/controllers/addUpdateTemplates.js
+++ app_modules/templates/controllers/addUpdateTemplates.js
@@ -3,8 +3,6 @@ const TemplateService = require('../services/dbData')
 const __util = require('../../../lib/util')
 const __constants = require('../../../config/constants')
 const __logger = require('../../../lib/logger')
-// const TemplateParamValidationService = require('../services/paramValidation')
-// const templateParamValidationService = new TemplateParamValidationService()
 
 const RedisService = require('../../../lib/redis_service/redisService')
 
@@ -12,12 +10,10 @@ const addUpdateTemplates = (req, res) => {
   __logger.info('add update template API called')
   const validate = new ValidatonService()
   const templateService = new TemplateService()
-  // let wabaInformationId = ''
   let wabaPhoneNumber = ''
   validate.addUpdateTemplate(req.body)
     .then(data => templateService.getTemplateTableDataAndWabaId(req.body.messageTemplateId, req.user.user_id))
     .then(wabaAndTemplateData => {
-      // wabaInformationId = wabaAndTemplateData.wabaInformationId
       wabaPhoneNumber = wabaAndTemplateData.wabaPhoneNumber
       if (wabaAndTemplateData.messageTemplateId) {
         return templateService.updateTemplateData(req.body, wabaAndTemplateData, req.user.user_id)
@@ -29,7 +25,6 @@ const addUpdateTemplates = (req, res) => {
     .then(data => {
       const redisService = new RedisService()
       redisService.setTemplatesInRedisForWabaPhoneNumber(wabaPhoneNumber)
-      // templateParamValidationService.setTemplatesInRedisForWabaId(wabaInformationId)
       __util.send(res, { type: __constants.RESPONSE_MESSAGES.SUCCESS, data: { mediaTemplateComplete: data } })
     })
     .catch(err => {
