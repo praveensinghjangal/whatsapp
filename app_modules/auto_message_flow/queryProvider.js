4cbc63cd3ab009161d759b0cb07e3d0e95d966db
Danish Galiyara <danish.galiyara@vivainfomedia.com>
2020-08-13 12:55:08 +0530

    added order by in identifier text

--- app_modules/auto_message_flow/queryProvider.js
+++ app_modules/auto_message_flow/queryProvider.js
@@ -14,7 +14,8 @@ const getMoreMenuByParentIdentifier = () => {
   return `select identifier_text as "identifierText",event, event_data as "eventData", identifier_text_name as identifierTextName
   from auot_message_flows amf 
   where is_active = true and waba_phone_number = ?
-  and lower(parent_identifier_text) = ?`
+  and lower(parent_identifier_text) = ?
+  order by identifierText`
 }
 
 const addEventTransaction = () => {
