9930ccd94f22ae3e5eca7f6c0051c62a719ae666
Arjun Bhole <arjun.bhole@vivainfomedia.com>
2020-08-17 13:07:58 +0530

    reverted validation 200 code changes

--- config/apiResponse.js
+++ config/apiResponse.js
@@ -280,10 +280,5 @@ module.exports = {
     status_code: 400,
     code: 3033,
     message: 'You\'ve exceeded the allowed limit please try again after some time'
-  },
-  FLOW_MANAGER_INVALID_REQUEST: {
-    status_code: 200,
-    code: 2000,
-    message: 'Invalid request'
   }
 }
