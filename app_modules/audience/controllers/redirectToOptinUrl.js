e2e27cb64d875df24db7ba7b0ea68b8b495220c9
JavedKh11 <khanjaved0413@gmail.com>
2020-09-22 12:12:28 +0530

    added redirectToOptinURl api and changes in swagger documentation

---
+++ app_modules/audience/controllers/redirectToOptinUrl.js
@@ -0,0 +1,21 @@
+const __constants = require('../../../config/constants')
+const __logger = require('../../../lib/logger')
+const RedisService = require('../../../lib/redis_service/redisService')
+
+const redirectToOptinUrl = (req, res) => {
+  __logger.info('redirectToOptinUrl::>>>>>>>>>>>>>>>..', req.params.wabaNumber)
+  const redisService = new RedisService()
+  redisService.getWabaDataByPhoneNumber(req.params.wabaNumber)
+    .then((data) => {
+      __logger.info('got Optin text----', data.optinText)
+      res.redirect(`${__constants.OPTIN_URL}/${req.params.wabaNumber}?text=${data.optinText}`)
+    })
+    .catch(err => {
+      __logger.error('error: ', err)
+      return res.status(403).send()
+    })
+}
+
+module.exports = {
+  redirectToOptinUrl
+}
