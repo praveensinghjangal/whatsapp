d0fee18fc8f8f46afb1fa58943975a70c12d3b7e
Arjun Bhole <arjun.bhole@vivainfomedia.com>
2020-12-28 12:59:09 +0530

    Made pan card regex case insensitive

--- config/constants.js
+++ config/constants.js
@@ -205,7 +205,7 @@ const VALIDATOR = {
   fileExtType: /^(jpg|jpeg|png)$/,
   url: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
   gst: /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/,
-  pan: /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/
+  pan: /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/i
 }
 const CHAT_APP_ENDPOINTS = {
   chatFlow: '/helowhatsappchat/api/flowmessage/chat',
