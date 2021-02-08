e0935dde2464ee9a7b7f3d70e8f2580ae6dbfed0
Vasim Gujrati <vasim.gujrati@vivaconnect.co>
2021-02-08 11:28:54 +0530

    changes of route sequence

--- app_modules/front_end/route.js
+++ app_modules/front_end/route.js
@@ -9,11 +9,11 @@ router.get('/addUpdateOptinMessageAndTemplate', authMiddleware.authenticate(auth
 
 // api wrapper
 router.get('/flow', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').getMenuBasedTemplateList)
-router.get('/flow/:flowTopicId', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').getFlow)
+router.post('/flow', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').flow)
 router.get('/flow/categories', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').getCategory)
+router.get('/flow/:flowTopicId', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').getFlow)
 router.delete('/flow/:flowTopicId', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').deleteEntireFlow)
 router.get('/flow/:flowTopicId/:identifierText', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').getIdentifier)
-router.post('/flow', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').flow)
 router.patch('/flow/:flowTopicId/active/:active', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').activeTemplate)
 router.patch('/flow/:flowTopicId/evaluate/:evaluationResponse', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').evaluationResult)
 router.delete('/flow/:flowTopicId/:identifierText', authMiddleware.authenticate(authstrategy.jwt.name, authstrategy.jwt.options), require('./wrapper/chatAppWrapper').deleteIdentifier)
