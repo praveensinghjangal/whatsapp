25732012915a5ced88ed22e84614fc89892ea367
Danish Galiyara <danish.galiyara@vivainfomedia.com>
2021-10-08 15:20:38 +0530

    Update sendIncomingAndStatusData.js

--- app_modules/web_hooks/controllers/sendIncomingAndStatusData.js
+++ app_modules/web_hooks/controllers/sendIncomingAndStatusData.js
@@ -4,28 +4,20 @@ const __db = require('../../../lib/db')
 const __constants = require('../../../config/constants')
 const q = require('q')
 
+const sendToQueue = (data, wabaNumber) => {
+  __logger.info('sendToTyntecQueue', data,wabaNumber)
+  const messageRouted = q.defer()
+  data.wabaNumber = wabaNumber || 0
+    __db.rabbitmqHeloWhatsapp.sendToQueue(data && data.contacts && data.messages ? __constants.MQ.fbIncoming : __constants.MQ.fbMessageStatus, JSON.stringify(data)) //todo : add check for status and if not status data then push to 3rd arbitrary queue
+      .then(queueResponse => messageRouted.resolve(true))
+      .catch(err => messageRouted.reject(err))
+  
+  return messageRouted.promise
+}
+
 module.exports = (req, res) => {
   sendToQueue(req.body, req.params.wabanumber)
     .then(data => __util.send(res, { type: __constants.RESPONSE_MESSAGES.ACCEPTED, data: {} }))
     .catch(err => __util.send(res, { type: __constants.RESPONSE_MESSAGES.SERVER_ERROR, err: err }))
 }
 
-const sendToQueue = (data, wabaNumber) => {
-  __logger.info('sendToTyntecQueue', Object.keys(data))
-  const messageRouted = q.defer()
-  const statusCheck = Object.keys(data)
-  if (statusCheck.indexOf('contacts') !== -1) {
-    data = { ...data, wabaNumber: wabaNumber }
-    __db.rabbitmqHeloWhatsapp.sendToQueue(__constants.MQ.fbIncoming, JSON.stringify(data))
-      .then(queueResponse => {
-        messageRouted.resolve(true)
-      })
-      .catch(err => messageRouted.reject(err))
-  } else {
-    data = { ...data, wabaNumber: wabaNumber }
-    __db.rabbitmqHeloWhatsapp.sendToQueue(__constants.MQ.fbMessageStatus, JSON.stringify(data))
-      .then(queueResponse => messageRouted.resolve(true))
-      .catch(err => messageRouted.reject(err))
-  }
-  return messageRouted.promise
-}
