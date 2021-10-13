24fc5e42c3b3565a783c892dde060d94490a20e9
Vivek Thakkar <thakkarv29@gmail.com>
2021-10-13 11:56:37 +0530

    saveoptin removed from audienceAndOptin as this is already being done in addAndUpdateAudience api which is being called inside it.

--- app_modules/integration/service/audienceAndOptin.js
+++ app_modules/integration/service/audienceAndOptin.js
@@ -3,7 +3,6 @@ const __config = require('../../../config')
 const __logger = require('../../../lib/logger')
 const __constants = require('../../../config/constants')
 const request = require('request')
-const integrationService = require('../../../app_modules/integration')
 
 const checkOptinMessage = (content, optinText) => {
   __logger.info('checkOptinMessage::>>>>>>>>>>>>>..')
@@ -18,34 +17,6 @@ const checkOptinMessage = (content, optinText) => {
   } else {
     isOptin.resolve(false)
   }
-  return isOptin.promise
-}
-
-const checkOptinForFacebook = (content, optinText, servicProviderId, maxTpsToProvider, userId, wabaPhoneNumber, phoneNumber) => {
-  const isOptin = q.defer()
-  checkOptinMessage(content, optinText)
-    .then((isoptin) => {
-      if (isoptin) {
-        const audienceService = new integrationService.Audience(servicProviderId, maxTpsToProvider, userId)
-        if (phoneNumber.indexOf('+') === -1) {
-          phoneNumber = `+${phoneNumber}`
-        }
-        audienceService.saveOptin(wabaPhoneNumber, [phoneNumber])
-          .then(responseData => {
-            if (responseData && responseData.data && responseData.data.length === 0) {
-              // "invalid phone number" array is empty. Therefore it is a valid phone number
-              isOptin.resolve(true)
-            } else {
-              isOptin.resolve(false)
-            }
-          })
-          .catch(err => {
-            isOptin.reject(err)
-          })
-      } else {
-        isOptin.resolve(false)
-      }
-    })
   return isOptin.promise
 }
 
@@ -67,23 +38,15 @@ function addAudienceAndOptin (inputPayload, redisData) {
     wabaPhoneNumber: redisData.id
   }]
   if (!audienceDataToBePosted[0].name) delete audienceDataToBePosted[0].name
-  if (__config.provider_config[redisData.serviceProviderId].name === 'tyntec') {
+  const providers = ['tyntec', 'facebook']
+  if (providers.indexOf(__config.provider_config[redisData.serviceProviderId].name) !== -1) {
     isOptinMessage = checkOptinMessage
-  } else if (__config.provider_config[redisData.serviceProviderId].name === 'facebook') {
-    isOptinMessage = checkOptinForFacebook
   }
-  isOptinMessage(inputPayload.content, redisData.optinText, redisData.serviceProviderId, redisData.maxTpsToProvider, redisData.userAccountIdByProvider, audienceDataToBePosted[0].wabaPhoneNumber, audienceDataToBePosted[0].phoneNumber)
+  isOptinMessage(inputPayload.content, redisData.optinText)
     .then(isoptin => {
       if (isoptin) {
         audienceDataToBePosted[0].optin = true
         audienceDataToBePosted[0].optinSourceId = __config.optinSource.message
-        if (__config.provider_config[redisData.serviceProviderId].name === 'facebook') {
-          audienceDataToBePosted[0].isFacebookVerified = true
-        }
-      } else {
-        if (__config.provider_config[redisData.serviceProviderId].name === 'facebook') {
-          audienceDataToBePosted[0].isFacebookVerified = false
-        }
       }
       const options = {
         url,
