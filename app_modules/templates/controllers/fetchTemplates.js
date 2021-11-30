6b3e52c7d4b0c018a2feee9eab73ed1ee981cdbd
vasim <vasim.gujrati@gmail.com>
2021-11-30 12:54:29 +0530

    changes in code for fetch template with counts and isPersonalized

--- app_modules/templates/controllers/fetchTemplates.js
+++ app_modules/templates/controllers/fetchTemplates.js
@@ -60,6 +60,10 @@ const getTemplateInfo = (req, res) => {
       __logger.info('data then 2', { data })
       finalResult[0].isTemplateValid = data.complete
       finalResult[0].invalidRemark = data.err && data.err.err ? data.err.err : null
+      finalResult[0] = JSON.stringify(finalResult[0])
+      finalResult[0] = finalResult[0].replace(/{{\d{1,3}}}/g, (match, key) => '<var' + match.slice(2).slice(0, -2) + '>')
+      finalResult[0] = JSON.parse(finalResult[0]) || {}
+      finalResult[0].isPersonalised = !!((finalResult[0] && finalResult[0].bodyTextVarExample && finalResult[0].headerTextVarExample && (finalResult[0].bodyTextVarExample.length > 0 || finalResult[0].headerTextVarExample.length > 0)))
       return checksForTemplate(finalResult[0])
     })
     .then(data => __util.send(res, { type: __constants.RESPONSE_MESSAGES.SUCCESS, data: finalResult }))
