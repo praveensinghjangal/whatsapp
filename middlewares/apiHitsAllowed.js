0e178d9f81c92d104b598d557cd1d9a092fd316c
vasim <vasim.gujrati@gmail.com>
2021-12-09 12:41:58 +0530

    added changes in ratelimitter and SendMessage

--- middlewares/apiHitsAllowed.js
+++ middlewares/apiHitsAllowed.js
@@ -8,14 +8,25 @@ const __logger = require('../lib/logger')
 const rateLimit = (req, res, next) => {
   __logger.info('Request>>>>>>>>>>>>>>>>>>>>>>>>..', req.userConfig)
   if (!req.userConfig) return next()
+  let consumekey = req.user.user_id
   if (req.userConfig.tps) {
+    const routeUrl = req.originalUrl.split('/')
+    if (routeUrl[routeUrl.length - 1] === __constants.MESSAGES) {
+      req.userConfig.routeUrl = routeUrl
+      req.userConfig.tps = 1
+      consumekey += '_mb'
+    } else if (routeUrl[routeUrl.length - 1] === __constants.MESSAGE) {
+      req.userConfig.routeUrl = routeUrl
+      req.userConfig.tps = 500
+      consumekey += '_ms'
+    }
     this.noOfHitsAllowedConfig = new RateLimiterRedis({
       storeClient: redisConnectionObject,
       keyPrefix: 'TPS',
       points: req.userConfig.tps, // 10 requests
       duration: 1 // per sec
     })
-    this.noOfHitsAllowedConfig.consume(req.user.user_id)
+    this.noOfHitsAllowedConfig.consume(consumekey)
       .then(rate => next())
       .catch(err => __util.send(res, { type: err.type || __constants.RESPONSE_MESSAGES.LIMIT_EXCEEDED, data: {}, err: err }))
   } else {
