6a11747a0abad53fddcd72aba76b9b6cccef9c4c
Danish Galiyara <danish.galiyara@vivainfomedia.com>
2022-04-19 17:07:29 +0530

    changes in docker file to install aws cli

---
+++ shell_scripts/launch_server/launch.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+echo $1
+echo $2
+echo $3
+echo "spawnning server"
+sleep 2
+echo "configuring server"
+sleep 2
+echo "spawnning fb containers"
+sleep 2
+echo "all done"
+echo "private IP = 10.40.13.240" >> shell_scripts/launch_server/logs/$3.log
+
