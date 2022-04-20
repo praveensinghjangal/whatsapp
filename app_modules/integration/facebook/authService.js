5efc5eb76ba4e471052d8126d618e4226a16f800
Vivek Thakkar <thakkarv29@gmail.com>
2022-04-20 17:39:54 +0530

    wabalogin api handled for set new password

--- app_modules/integration/facebook/authService.js
+++ app_modules/integration/facebook/authService.js
@@ -9,20 +9,28 @@ const WabaService = require('../../whatsapp_business/services/businesAccount')
 const RedisService = require('../../../lib/redis_service/redisService')
 
 class InternalFunctions {
-  WabaLoginApi (username, password, url, graphApiKey, userAccountIdByProvider, wabaNumber, userId) {
+  WabaLoginApi (username, password, newPassword, url, graphApiKey, userAccountIdByProvider, wabaNumber, userId, saveInRedisAndDb) {
     const apiCalled = q.defer()
     const http = new HttpService(60000)
     const headers = { Authorization: `Basic ${Buffer.from(`${username}:${password}`).toString('base64') || ''}` }
     const resolveObj = { graphApiKey: graphApiKey, baseUrl: url, userAccountIdByProvider }
-    http.Post({}, 'body', url + __constants.FACEBOOK_ENDPOINTS.login, headers, __config.service_provider_id.facebook)
+    const body = {}
+    if (newPassword) {
+      body.new_password = newPassword
+    }
+    http.Post(body, 'body', url + __constants.FACEBOOK_ENDPOINTS.login, headers, __config.service_provider_id.facebook)
       .then(data => {
         __logger.info('post metadata api response', data)
         data = data.body || data
         if (data && data.users && data.users.length > 0 && data.users[0].token) {
           resolveObj.apiKey = data.users[0].token
           resolveObj.timeLeftToExpire = +moment(data.users[0].expires_after).format('x') - new Date().getTime()
-          const wabaService = new WabaService()
-          return wabaService.updateWabizApiKeyAndExpireyTime(wabaNumber, data.users[0].token, data.users[0].expires_after, userId)
+          if (saveInRedisAndDb) {
+            const wabaService = new WabaService()
+            return wabaService.updateWabizApiKeyAndExpireyTime(wabaNumber, data.users[0].token, data.users[0].expires_after, userId)
+          } else {
+            return resolveObj
+          }
         } else {
           return rejectionHandler({ type: __constants.RESPONSE_MESSAGES.ACCESS_DENIED, err: { } })
         }
@@ -46,7 +54,7 @@ class Authentication {
         const timeLeftToExpire = wabaData.wabizApiKeyExpiresOn ? +moment(wabaData.wabizApiKeyExpiresOn).format('x') - new Date().getTime() : 0
         if (timeLeftToExpire < __constants.FB_REDIS_KEY_BUFFER_TIME) {
           const internalFunctions = new InternalFunctions()
-          return internalFunctions.WabaLoginApi(wabaData.wabizUsername, wabaData.wabizPassword, wabaData.wabizBaseUrl, wabaData.graphApiKey, wabaData.userAccountIdByProvider, wabaNumber, this.userId)
+          return internalFunctions.WabaLoginApi(wabaData.wabizUsername, wabaData.wabizPassword, null, wabaData.wabizBaseUrl, wabaData.graphApiKey, wabaData.userAccountIdByProvider, wabaNumber, this.userId, true)
         } else {
           return { baseUrl: wabaData.wabizBaseUrl, apiKey: wabaData.apiKey, graphApiKey: wabaData.graphApiKey, userAccountIdByProvider: wabaData.userAccountIdByProvider, timeLeftToExpire }
         }
