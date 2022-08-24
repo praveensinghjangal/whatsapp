ec232dd4dec549f78ad52d4ed56a54ffdab622ca
gopalkrishna.kothari <gopalkrishna.kothari@vivaconnect.co>
2022-08-24 11:37:20 +0530

    Validation added in reports api

--- app_modules/reports/services/validation.js
+++ app_modules/reports/services/validation.js
@@ -16,22 +16,23 @@ class validate {
       properties: {
         consumerNumber: {
           type: 'string',
-          required: false,
           minLength: 6,
           maxLength: 15,
           pattern: __constants.VALIDATOR.number
         },
         messageId: {
           type: 'string',
-          required: false,
           minLength: 1,
           maxLength: 50
         },
-        campaignName: {
-          type: 'string',
-          required: false,
-          minLength: 1,
-          maxLength: 50
+        status: {
+          type: 'array',
+          minItems: 1,
+          items: {
+            type: 'string',
+            minLength: 1,
+            maxLength: 50
+          }
         },
         startDate: {
           type: 'string',
@@ -43,16 +44,49 @@ class validate {
           required: true,
           pattern: __constants.VALIDATOR.timeStamp,
           minLength: 1
+        },
+        limit: {
+          type: 'string',
+          required: false,
+          pattern: __constants.VALIDATOR.number
+        },
+        page: {
+          type: 'string',
+          required: false,
+          pattern: __constants.VALIDATOR.number
         }
-      }
+      },
+      oneOf: [
+        {
+          required: ['consumerNumber']
+        },
+        {
+          required: ['messageId']
+        },
+        {
+          required: ['status']
+        }
+      ],
+      additionalProperties: false
     }
     const formatedError = []
     v.addSchema(schema, '/deliveryReport')
     const error = _.map(v.validate(request, schema).errors, 'stack')
     _.each(error, function (err) {
       const formatedErr = err.split('.')
-      const regexPatternPreetyMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid date format- use yyyy-mm-dd hh:MM:ss')
-      formatedError.push(regexPatternPreetyMessage)
+      if (err.includes('does not match pattern "^[0-9]+$"')) {
+        var regexPatternPreetyPaginationMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid number')
+        formatedError.push(regexPatternPreetyPaginationMessage)
+      } else if (err.includes('does not match pattern "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$"')) {
+        var regexPatternPreetyMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid date format- use yyyy-mm-dd hh:MM:ss')
+        formatedError.push(regexPatternPreetyMessage)
+      } else if (err.includes('instance is not any of')) {
+        formatedError.push('Please provide atleast one field consumerNumber, messageId', 'status')
+      } else if (err.includes('instance is not exactly one from [subschema 0],[subschema 1]')) {
+        formatedError.push('Please provide either consumerNumber, messageId or status')
+      } else {
+        formatedError.push(formatedErr[formatedErr.length - 1])
+      }
     })
     if (formatedError.length > 0) {
       isvalid.reject({ type: __constants.RESPONSE_MESSAGES.INVALID_REQUEST, err: formatedError })
@@ -95,6 +129,16 @@ class validate {
           required: true,
           pattern: __constants.VALIDATOR.timeStamp,
           minLength: 1
+        },
+        limit: {
+          type: 'string',
+          required: false,
+          pattern: __constants.VALIDATOR.number
+        },
+        page: {
+          type: 'string',
+          required: false,
+          pattern: __constants.VALIDATOR.number
         }
       }
     }
@@ -103,8 +147,15 @@ class validate {
     const error = _.map(v.validate(request, schema).errors, 'stack')
     _.each(error, function (err) {
       const formatedErr = err.split('.')
-      const regexPatternPreetyMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid date format- use yyyy-mm-dd hh:MM:ss')
-      formatedError.push(regexPatternPreetyMessage)
+      if (err.includes('does not match pattern "^[0-9]+$"')) {
+        var regexPatternPreetyPaginationMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid number')
+        formatedError.push(regexPatternPreetyPaginationMessage)
+      } else if (err.includes('does not match pattern "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$"')) {
+        var regexPatternPreetyMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid date format- use yyyy-mm-dd hh:MM:ss')
+        formatedError.push(regexPatternPreetyMessage)
+      } else {
+        formatedError.push(formatedErr[formatedErr.length - 1])
+      }
     })
     if (formatedError.length > 0) {
       isvalid.reject({ type: __constants.RESPONSE_MESSAGES.INVALID_REQUEST, err: formatedError })
@@ -133,13 +184,11 @@ class validate {
       properties: {
         templateId: {
           type: 'string',
-          required: false,
           minLength: 1,
           maxLength: 50
         },
         templateName: {
           type: 'string',
-          required: false,
           minLength: 1,
           maxLength: 50
         },
@@ -153,16 +202,46 @@ class validate {
           required: true,
           pattern: __constants.VALIDATOR.timeStamp,
           minLength: 1
+        },
+        limit: {
+          type: 'string',
+          required: false,
+          pattern: __constants.VALIDATOR.number
+        },
+        page: {
+          type: 'string',
+          required: false,
+          pattern: __constants.VALIDATOR.number
         }
-      }
+      },
+      oneOf: [
+        {
+          required: ['templateId']
+        },
+        {
+          required: ['templateName']
+        }
+      ],
+      additionalProperties: false
     }
     const formatedError = []
     v.addSchema(schema, '/templateSummaryReport')
     const error = _.map(v.validate(request, schema).errors, 'stack')
     _.each(error, function (err) {
       const formatedErr = err.split('.')
-      const regexPatternPreetyMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid date format- use yyyy-mm-dd hh:MM:ss')
-      formatedError.push(regexPatternPreetyMessage)
+      if (err.includes('does not match pattern "^[0-9]+$"')) {
+        var regexPatternPreetyPaginationMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid number')
+        formatedError.push(regexPatternPreetyPaginationMessage)
+      } else if (err.includes('does not match pattern "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$"')) {
+        var regexPatternPreetyMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid date format- use yyyy-mm-dd hh:MM:ss')
+        formatedError.push(regexPatternPreetyMessage)
+      } else if (err.includes('instance is not any of')) {
+        formatedError.push('Please provide atleast one field templateId, templateName')
+      } else if (err.includes('instance is not exactly one from [subschema 0],[subschema 1]')) {
+        formatedError.push('Please provide either templateId or templateName')
+      } else {
+        formatedError.push(formatedErr[formatedErr.length - 1])
+      }
     })
     if (formatedError.length > 0) {
       isvalid.reject({ type: __constants.RESPONSE_MESSAGES.INVALID_REQUEST, err: formatedError })
@@ -262,6 +341,16 @@ class validate {
           required: true,
           pattern: __constants.VALIDATOR.timeStamp,
           minLength: 1
+        },
+        limit: {
+          type: 'number',
+          required: false,
+          pattern: __constants.VALIDATOR.number
+        },
+        page: {
+          type: 'number',
+          required: false,
+          pattern: __constants.VALIDATOR.number
         }
       }
     }
@@ -270,8 +359,16 @@ class validate {
     const error = _.map(v.validate(request, schema).errors, 'stack')
     _.each(error, function (err) {
       const formatedErr = err.split('.')
-      const regexPatternPreetyMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid date format- use yyyy-mm-dd hh:MM:ss')
-      formatedError.push(regexPatternPreetyMessage)
+      console.log('------------------------', formatedErr)
+      if (err.includes('does not match pattern "^[0-9]+$"')) {
+        var regexPatternPreetyPaginationMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid number')
+        formatedError.push(regexPatternPreetyPaginationMessage)
+      } else if (err.includes('does not match pattern "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$"')) {
+        var regexPatternPreetyMessage = formatedErr[1].split(' "^')[0].replace('does not match pattern', '- invalid date format- use yyyy-mm-dd hh:MM:ss')
+        formatedError.push(regexPatternPreetyMessage)
+      } else {
+        formatedError.push(formatedErr[formatedErr.length - 1])
+      }
     })
     if (formatedError.length > 0) {
       isvalid.reject({ type: __constants.RESPONSE_MESSAGES.INVALID_REQUEST, err: formatedError })
