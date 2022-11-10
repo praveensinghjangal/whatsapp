b381b3287801824a786f367d0cd50f14673c908e
santosh.sahu <santosh.sahu@vivaconnect.co>
2022-11-10 17:43:51 +0530

    mis changes

--- workers/schedulers/monthMailConversationService.js
+++ workers/schedulers/monthMailConversationService.js
@@ -23,6 +23,17 @@ const bodyCreator = (array) => {
   const arraydata = Object.values(merged)
   return arraydata
 }
+const bodyCreator1 = (array) => {
+  const merged = array.reduce((r, { wabaPhoneNumber, messageCountry, ...rest }) => {
+    const key = `${wabaPhoneNumber}-${messageCountry}`
+    r[key] = r[key] || { wabaPhoneNumber, messageCountry, ui: 0, bi: 0, rc: 0, na: 0 }
+    r[key][rest.conversationCategory] = rest.conversationCategoryCount
+    r[key].total = r[key].total ? r[key].total + rest.conversationCategoryCount : rest.conversationCategoryCount
+    return r
+  }, {})
+  const arraydata = Object.values(merged)
+  return arraydata
+}
 
 // handle no record for mis data as of now mis stops in case of no data but if there is 0 campagin the opt out data should go
 const messageStatusOnMailForConversation = () => {
@@ -56,83 +67,124 @@ const messageStatusOnMailForConversation = () => {
       }
     })
     .then((dbResponse)=>{
-      console.log("_________----------------",dbResponse)
-     if(dbResponse)
+      console.log("111111111111111111111111111111111111",dbResponse)
+     if(dbResponse && dbResponse.length)
      {
       allUserData = bodyCreator(dbResponse)
       console.log("alluserData",allUserData)
       return dbService.getWabaNameByWabaNumberWithCode(arrayofWabanumber)
-
-     }else{
-   
-
      }
-      
     })
-    .then(dbresponse => {
-       
-        for (let i = 0; i < dbresponse.length; i++) {
-          userIdToUserName[dbresponse[i].wabaPhoneNumber] = dbresponse[i].businessName
-        }
-        console.log("userIdToUserName", userIdToUserName)
-      })
+    .then(dbwabaName => {
+      console.log('222222222222222222222222222222222222222222222222',dbwabaName)
+      if (dbwabaName && dbwabaName.length) {
+      for (let i = 0; i < dbwabaName.length; i++) {
+        userIdToUserName[dbwabaName[i].wabaPhoneNumber] = dbwabaName[i].businessName
+      }
+      console.log("userIdToUserName", userIdToUserName)
+    } else {
+      // need to develop
+    }
+    })
     .then (()=>{
-      console.log("length",allUserData,allUserData.length)
        for (let i = 0; i < allUserData.length; i++) {
          data = allUserData[i]
          if(data.wabaPhoneNumber){
           data['businessName'] = userIdToUserName[data.wabaPhoneNumber]
-         } 
+         } else {
+           // need to develop
+         }
         }
-        console.log('data......',allUserData)
+        console.log('3333333333333333333333333333333333',allUserData)
         return allUserData
-
     })
-    .then((data)=>{
-         // const allUserGrouped = _.groupBy(allUserData ,item => {
-        //   return [item['businessName']];
-      // });
-      // const groupBybusinessName = _.groupBy(data, item => item.businessName)
-      // console.log("________________---------------------------__ppp",groupBybusinessName)
-      // console.log("-------------",groupBybusinessName)
-      const groupBybusinessName = _.groupBy(data, item => item.businessName)
-      _.each(businessName, (singleUserData, wabaKey) => {
-        const alluserCountryLastDay = _.groupBy(singleUserData, item => item.countryOfPhoneNumber)
+    .then(()=>{
+      let allUserGroupedOnBusinessName = _.groupBy(allUserData ,item => {
+              return [item['businessName']];
+      })
+      console.log('3333333333333333333333333333333333333333333333333',allUserGroupedOnBusinessName)
+      // Object.keys(allUserGroupedOnBusinessName).forEach((keys)=>{
+      //   console.log('111111111111111111111',[keys])
+      // })
+      let groupNumber
+      const  objectKeys = Object.keys(allUserGroupedOnBusinessName)
+      const obj= []
+      const obj1 = {}
+      for(let i =0; i<objectKeys.length;i++){
+        const b = objectKeys[i]
+        const c = allUserGroupedOnBusinessName[b]
+        groupNumber =_.groupBy(c ,item => {
+                return[item['wabaPhoneNumber']];
+            });
+            obj1[b] = [groupNumber]
+            console.log('99999999999999999999999999999999999999999',obj1)
+            
+            
+          }
+          obj.push(obj1)
+          console.log("-------------------------------obj",obj)
+        console.log("groupNumber-------------------------------------",groupNumber)
+        return obj
+    }).then((groupNumber)=>{
 
-        // let emptyjsonLastDay = {}
-        // _.each(alluserCountryLastDay, (singlecountryData, key) => {
-        //   UserLastDayDataArr = [key, [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], 0]
+      //console.group("4444444444444444444444",obj1,allUserGroupedOnBusinessName)
+    const emailService = new EmailService(__config.emailProvider)
+    const subject = `MIS Report for ${preMonth}` 
+    return emailService.sendEmail(__config.misEmailList, subject, emailTemplates.messageAndConvoMisMonth(groupNumber))
+})
+    // .then((data)=>{
+    //      // const allUserGrouped = _.groupBy(allUserData ,item => {
+    //     //   return [item['businessName']];
+    //   // });
+    //   // const groupBybusinessName = _.groupBy(data, item => item.businessName)
+    //   // console.log("________________---------------------------__ppp",groupBybusinessName)
+    //    console.log("11111111111111111111111111111111111111111111111",allUserData)
+    //   const groupBybusinessName = _.groupBy(data, item => item.businessName )
+    //   _.each(groupBybusinessName, (singleUserData, wabaKey) => {
+    //     const groupNumber = _.groupBy(singleUserData, item => item.wabaPhoneNumber)
+      
+    //      console.log("66666666666666666666",singleUserData[wabaKey])
+    //   })
+    //   console.log("11111111111111111111111111111111",groupBybusinessName.wabaKey)
+    //   process.exit(1)
+
+       
+
+    //   //   const allUserLastDay = _.groupBy(lastDayData, item => item.wabaPhoneNumber)
+    //   // _.each(allUserLastDay, (singleUserData, wabaKey) => {
+    //   //   const alluserCountryLastDay = _.groupBy(singleUserData, item => item.countryOfPhoneNumber)
+
+    //     // let emptyjsonLastDay = {}
+    //     // _.each(alluserCountryLastDay, (singlecountryData, key) => {
+    //     //   UserLastDayDataArr = [key, [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], 0]
   
-        return groupBybusinessName
-    })
-    .then ((data)=>{
+    //    // return groupBybusinessName
+    // })
+    // .then ((data)=>{
 
-      function createRowHTML(){
-        var tableContent=""
-        for(i=0; i<data.length;i++){
-        var rowspan = 0;
-        var detailLength = data.key.length;
-        rowspan = detailLength;
+    //   function createRowHTML(){
+    //     var tableContent=""
+    //     for(i=0; i<data.length;i++){
+    //     var rowspan = 0;
+    //     var detailLength = data.key.length;
+    //     rowspan = detailLength;
       
-        tableContent += "<tr><td rowspan=" + parseInt(1 + rowspan) + ">" + data.key.getActiveBusinessNumber + "</td></tr>";
+    //     tableContent += "<tr><td rowspan=" + parseInt(1 + rowspan) + ">" + data.key.getActiveBusinessNumber + "</td></tr>";
       
-        // data.map((ele) => {
-        //   // '<td>'ele.ui'</td>'
-        //   // <td>ele.bi</td>
-        //   // <td>ele.rc</td>
-        //   // <td>ele.na</td>
-        //   // <td>ele.total</td>
-        // })
+    //     // data.map((ele) => {
+    //     //   // '<td>'ele.ui'</td>'
+    //     //   // <td>ele.bi</td>
+    //     //   // <td>ele.rc</td>
+    //     //   // <td>ele.na</td>
+    //     //   // <td>ele.total</td>
+    //     // })
 
-      }
-    }
+    //   }
+    // }
 
 
       //console.log(data)
-      const emailService = new EmailService(__config.emailProvider)
-        const subject = `MIS Report for ${preMonth}` 
-        return emailService.sendEmail(__config.misEmailList, subject, emailTemplates.messageAndConvoMisMonth(data))
-    })
+ 
   // dbService.getMisRelatedData(startOfMonth, endOfMonth)
     // .then(responseFromDb => {
     //   const arrayofWabanumber = []
