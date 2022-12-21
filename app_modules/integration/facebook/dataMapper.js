1e11c219aad05271b91d43b100c100e4c7e8a3e2
Rahul Wala <rahul.wala@vivaconnect.co>
2022-12-21 11:10:13 +0530

    Added log in data mapper to get generated fb req body

--- app_modules/integration/facebook/dataMapper.js
+++ app_modules/integration/facebook/dataMapper.js
@@ -413,6 +413,7 @@ class DataMapper {
     const internalService = new InternalService()
     internalService.sendMessageFbBody(data, maxTpsToProvider)
       .then(body => {
+        __logger.info('dataMapper: sendMessage(): fb req body:', JSON.stringify(body))
         deferred.resolve(body)
       })
       .catch(err => {
