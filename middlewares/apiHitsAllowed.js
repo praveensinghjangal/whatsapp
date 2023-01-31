e7a3b7896daeadd3a93332932cd0d64e3db047b4
Rahul Wala <rahul.wala@vivaconnect.co>
2023-01-31 12:54:42 +0530

    Changed apiHitsAllowed error log

--- middlewares/apiHitsAllowed.js
+++ middlewares/apiHitsAllowed.js
@@ -34,7 +34,7 @@ const rateLimit = (req, res, next) => {
       .then(rate => next())
       .catch(err => {
         __logger.error('apiHitsAllowed: rateLimit(): noOfHitsAllowedConfig.consume():', err, err.stack)
-        errorToTelegram.send(err, `apiHitsAllowed: rateLimit(): ${req.user.wabaPhoneNumber}: limit: ${__constants.RESPONSE_MESSAGES.LIMIT_EXCEEDED.message}`)
+        errorToTelegram.send({}, `apiHitsAllowed: rateLimit(): ${req.user.wabaPhoneNumber}: limit: ${__constants.RESPONSE_MESSAGES.LIMIT_EXCEEDED.message}`)
         __util.send(res, { type: err.type || __constants.RESPONSE_MESSAGES.LIMIT_EXCEEDED, data: {}, err: err })
       })
   } else {
