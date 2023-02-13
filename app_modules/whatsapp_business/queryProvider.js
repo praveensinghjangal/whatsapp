043b93821a7dcaf0e9c91ba18892ef3e7a2eb646
gopalkrishna.kothari <gopalkrishna.kothari@vivaconnect.co>
2023-02-13 11:40:57 +0530

    removed unwanted code

--- app_modules/whatsapp_business/queryProvider.js
+++ app_modules/whatsapp_business/queryProvider.js
@@ -310,10 +310,6 @@ const getTotalUser = () => {
   where is_active = true and user_role_id != '${__constants.SUPPORT_ROLE_ID}'`
 }
 
-const getWabizDetails = () => {
-  return 'select wabiz_base_url as wabizBaseUrl, concat(phone_code, phone_number) as wabaPhoneNumber, business_name as businessName, api_key as apiKey from waba_information wi  where is_active = true order by created_on DESC'
-}
-
 module.exports = {
   getBusinessCategory,
   getBusinessProfile,
@@ -347,6 +343,5 @@ module.exports = {
   updateServiceProviderData,
   getServiceTotalProviderCount,
   updateWabizApiKeyAndExpireyTime,
-  getTotalUser,
-  getWabizDetails
+  getTotalUser
 }
